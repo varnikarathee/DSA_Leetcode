28d5fb191ed700f9d51ef9835186a785ee8c2fb6
Varnika Rathee <77229404+varnikarathee@users.noreply.github.com>
2023-01-08 23:16:28 +0530

    Time: 1 ms (100.00%), Space: 39.4 MB (84.70%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -7,13 +7,25 @@ class Solution {
             return false;
         }
         
-        while(n>2){
-            if(n%2!=0){
-                return false;
-            }
-            n=n/2;
-        }
-        return true;
+//         while(n>2){
+//             if(n%2!=0){
+//                 return false;
+//             }
+//             n=n/2;
+//         }
+//         return true;
+//     }
+        
+     return recur(n);   
     }
-   
+    
+    public boolean recur(int n){
+        if(n%2!=0){
+            return false;
+        }
+        if(n==2){
+            return true;
+        }
+        return recur(n/2);
+    }
 }
