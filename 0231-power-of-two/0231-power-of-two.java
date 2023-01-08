6f1d023353633a5d79d34355ad4c63e1cde03a18
Varnika Rathee <77229404+varnikarathee@users.noreply.github.com>
2023-01-08 23:02:55 +0530

    Time: 1 ms (100.00%), Space: 39.5 MB (80.07%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n==1){
+            return true;
+        }
+        if(n<=0){
+            return false;
+        }
+        
+        while(n>2){
+            if(n%2!=0){
+                return false;
+            }
+            n=n/2;
+        }
+        return true;
+    }
+   
+}
